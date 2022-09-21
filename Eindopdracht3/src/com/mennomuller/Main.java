4ee728b406f685abf59e8faf78dd97dae0eed373
MennoMuller <menno.muller.nl@gmail.com>
2022-09-21 10:16:20 +0200

    Eindopdracht les 3 versie 2

--- Eindopdracht3/src/com/mennomuller/Main.java
+++ Eindopdracht3/src/com/mennomuller/Main.java
@@ -15,7 +15,7 @@ public class Main {
                 number = input.nextInt();
                 if (number < 0) {
                     System.out.println("Getal negatief, fout\n");
-                }else if(number==0){
+                } else if (number == 0) {
                     System.out.println("Getal nul, fout\n");
                 } else if (number > 45) {
                     System.out.println("Getal te groot, past niet\n");
@@ -28,14 +28,14 @@ public class Main {
             }
         }
         System.out.println("De eerste " + number + " Lucas-getallen:");
-        System.out.println(lucasString(2,1,number));
+        System.out.println(lucasString(2, 1, number));
     }
 
-    public static String lucasString(int lucas1, int lucas2, int steps){
-        if(steps == 1){
+    public static String lucasString(int lucas1, int lucas2, int steps) {
+        if (steps == 1) {
             return Integer.toString(lucas1);
         } else {
-            return lucas1+" " + lucasString(lucas2,lucas1+lucas2,steps-1);
+            return lucas1 + " " + lucasString(lucas2, lucas1 + lucas2, steps - 1);
         }
     }
 }
